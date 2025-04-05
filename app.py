929ceac1648bf45f2711acb759030465e7dce7df
Vincent24-z <53919095+Vincent24-z@users.noreply.github.com>
2025-04-05 15:05:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ if uploaded_timecard and uploaded_tripreport:
     timecard_df['Clock Out'] = pd.to_datetime(timecard_df['Time Out'], format='%H:%M:%S', errors='coerce')
     timecard_df['Working Hours'] = (timecard_df['Clock Out'] - timecard_df['Clock In']).dt.total_seconds() / 3600
 
-trip_df['Drive Time'] = pd.to_timedelta(trip_df['Driving Duration'].apply(lambda x: x.strftime('%H:%M:%S') if pd.notnull(x) else pd.NaT))
+    trip_df['Drive Time'] = pd.to_timedelta(trip_df['Driving Duration'].apply(lambda x: x.strftime('%H:%M:%S') if pd.notnull(x) else pd.NaT))
 
 
 
